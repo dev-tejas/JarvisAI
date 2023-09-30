ba0b527766769d162ca5286496aafac702d374bb
Tejas <tejaschauhan.py@gmail.com>
2023-09-30 17:04:47 +0530

    Added Basic Speak Prototype

--- Functions.py
+++ Functions.py
@@ -1,8 +1,8 @@
 import pyttsx3
 
-engine = pyttsx3.init('SAPI5')
+engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voice[0].id)
+engine.setProperty('voice', voices[0].id)
 
 def speak(text):
     engine.say(text)
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
+from Functions import speak
 
+if __name__ == "__main__":
+    speak("Hello World")
