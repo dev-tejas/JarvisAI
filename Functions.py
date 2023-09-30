79d900a63421453a521151568d3196f7fa884204
Tejas <tejaschauhan.py@gmail.com>
2023-09-30 17:13:40 +0530

    Added takeCommand Function

--- Functions.py
+++ Functions.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -7,3 +8,22 @@ engine.setProperty('voice', voices[0].id)
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source)
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    
+    return query
