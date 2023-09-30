a8cac015f200e9261fc4503aa74176ff6903afe5
Tejas <tejaschauhan.py@gmail.com>
2023-09-30 17:24:15 +0530

    Added New Functions

--- Functions.py
+++ Functions.py
@@ -1,9 +1,11 @@
 import pyttsx3
 import speech_recognition as sr
+import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 145)
 
 def speak(text):
     engine.say(text)
@@ -27,3 +29,16 @@ def takeCommand():
         return "None"
     
     return query
+
+def greet():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from Functions import speak
+import Functions as func
+import wikipedia
+import datetime
 
 if __name__ == "__main__":
-    speak("Hello World")
+    func.greet()
+    while True:
+        query = func.takeCommand().lower()
+
+        if 'wikipedia' in query:
+                func.speak('Searching Wikipedia...')
+                query = query.replace("wikipedia", "")
+                results = wikipedia.summary(query, sentences=2)
+                func.speak("According to Wikipedia")
+                print(results)
+                func.speak(results)
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            func.speak(f"Sir, the time is {strTime}")
